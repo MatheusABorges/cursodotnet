8c772e1534687d4f58e1f48b4d15c9c59ba2a407
Matheus Araujo <matheusaraujob@dcc.ufrj.br>
2022-06-03 13:47:27 -0300

    DTO's e deleção de usuários adicionados

--- api/Controllers/AccountController.cs
+++ api/Controllers/AccountController.cs
@@ -5,8 +5,10 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using api.Data;
+using api.DTOs;
 using api.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -19,13 +21,15 @@ namespace api.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password){
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto){
+            if(await UserExists(registerDto.UserName)) return BadRequest("Username Already Exists");
+
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.UserName.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
 
@@ -34,6 +38,47 @@ namespace api.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+            var user = await _context.Users.SingleOrDefaultAsync(x => String.Equals(x.UserName, loginDto.UserName.ToLower()));
+
+            if(user == null) return Unauthorized("Invalid Username");
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i = 0; i < computedHash.Length; i++){
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+        
+            return user;
+        }
+               
+        [HttpDelete("delete/{id}")]
+        public async Task<ActionResult<AppUser>> DeleteUser(int id){
+            var user = await _context.Users.FindAsync(id);
+            if(user == null) return BadRequest("User does not exist");
+            _context.Attach(user);
+           _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        [HttpDelete("fastdelete/{id}")]
+        public async Task<ActionResult<bool>> FastDelete(int id){
+            var user = new AppUser{Id = id};
+            _context.Attach(user);
+           _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+
+        private async Task<bool> UserExists(string username){
+            return await _context.Users.AnyAsync(x => String.Equals(x.UserName,
+                                                                    username.ToLower()));
         }
     }
 }
